4126e11e9605bdd694b150fbf4db6f8175c1dcfe
bentshrimp <bent_shrimp@kookmin.ac.kr>
2026-05-20 15:12:52 +0900

    fix: entity field 수정, api 명세 추가, test 버그 수정

--- src/main/java/com/flowary/server/analysis/AnalysisFlower.java
+++ src/main/java/com/flowary/server/analysis/AnalysisFlower.java
@@ -1,6 +1,5 @@
 package com.flowary.server.analysis;
 
-import com.flowary.server.analysis.Analysis;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,10 +15,6 @@ public class AnalysisFlower {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "flower_id")
     private Long flowerId; // ERD: flower_id INT
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "analysis_id")
-    private Analysis analysis; // ERD: Analysis_id VARCHAR(50)
 
     @Column(name = "name_ko", nullable = false, length = 100)
     private String nameKo; // ERD: name_ko VARCHAR(100)
